97bc14332a63c20657db32b1dd4d6765d092e786
Aamir <82281117+aamirazad@users.noreply.github.com>
2022-10-16 21:48:31 -0400

    Day 1
    i did it

---
+++ Thirty-day-challange/dayOne/Hello.js
@@ -0,0 +1,6 @@
+// Welcome to 30DaysofJavaScript
+
+/* Comments can make code readable,
+ * easy to reuse,
+ * and informative
+
---
+++ Thirty-day-challange/dayOne/variable.js
@@ -0,0 +1,11 @@
+let cat = "Oreo";
+let computerAge = 10;
+let computerWorking = true;
+let time;
+let sleep = null;
+console.log(typeof cat);
+console.log(typeof computerAge);
+console.log(typeof computerWorking);
+console.log(typeof time);
+console.log(typeof sleep);
+
