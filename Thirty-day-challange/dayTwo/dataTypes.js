2ac0b40cd92a9d5b0e4ba7acce5a39fe614d5060
Aamir <82281117+aamirazad@users.noreply.github.com>
2022-10-18 16:36:36 -0400

    day 2 numbers section

---
+++ Thirty-day-challange/dayTwo/dataTypes.js
@@ -0,0 +1,43 @@
+let numOne = 3
+let numTwo = 3
+
+console.log(numOne == numTwo)
+
+let js = 'JavaScript'
+let py = "Python"
+
+console.log(js == py)
+
+let lightOn = true
+let lightOff = false
+
+console.log(lightOn == lightOff)
+
+let nums = [1, 2, 3]
+nums[0] = 10
+
+console.log(nums)
+
+let numsOne = [1, 2, 3]
+let numbers = [1, 2, 3]
+
+console.log(numsOne == numbers)
+
+let userOne = {
+	name:"Asabeneh",
+	role:"teaching",
+	country:"Finland"
+}
+
+let userTwo = {
+	name:"Asabeneh",
+	role:"teaching",
+	country:"Finland"
+}
+
+console.log(userOne == userTwo)
+
+let numsTwo = [1, 2, 3]
+let numbersOne = numsTwo
+
+console.log(numsTwo == numbersOne)
