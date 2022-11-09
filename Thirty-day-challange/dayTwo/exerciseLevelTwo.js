b2c5fdaf6954b1159bdba69b8e6411440619960a
Aamir Azad <aamirmazad@gmail.com>
2022-11-08 19:36:05 -0500

    codecademy time

--- Thirty-day-challange/dayTwo/exerciseLevelTwo.js
+++ Thirty-day-challange/dayTwo/exerciseLevelTwo.js
@@ -1,9 +1,9 @@
 let johnQuote = `'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.`
 console.log(johnQuote)
 let teresaQuote = "Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead."
-console.log(teresaQuote)
+console.log(teresaQuote);
 
-let number = '10'
+let number = '10';
 
 if (number == 10) {
 	console.log("Your good")
