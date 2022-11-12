bd644a278a3c1e71848826208a3f73cfe45446b5
Aamir Azad <aamirmazad@gmail.com>
2022-11-12 17:49:58 -0500

    Kinda redoing everything

--- Thirty-day-challange/dayTwo/numbers.js
+++ Thirty-day-challange/dayTwo/numbers.js
@@ -1,4 +1,14 @@
-let age = 35
-
 const PI = Math.PI 
 console.log(PI)
+const age = 13.8
+
+console.log(Math.round(PI))
+console.log(Math.floor(age))
+console.log(Math.round(age))
+
+console.log(-5, 3, 20, 4, 5, 10)
+console.log(Math.min(-5, 3, 20, 4, 5,10))
+const randNum = Math.random()
+console.log(randNum)
+
+
---
+++ Thirty-day-challange/dayTwo/strings.js
@@ -0,0 +1,21 @@
+let space = ' '
+let firstName = "Aamir"
+let lastName = "Azad"
+let country = "United States"
+let city = "Hollidaysburg"
+let language = "JavaScript"
+let job = "student"
+let age = 13
+
+let fullName = firstName + space + lastName;
+console.log(fullName);
+let personInfoOne = fullName + '. I am ' + age + '. I live in ' + country;
+console.log(personInfoOne)
+let a = 2
+let b = 3
+console.log(`The sum of ${age} and ${b} is ${a + b}`)
+let personInfoTwo = `I am ${fullName}, I am ${age}, I live in ${country}.`
+console.log(personInfoTwo)
+console.log(`${a} is greater than ${b}: ${a > b}`)
+let js = 'JavaScript'
+console.log(js.length);
