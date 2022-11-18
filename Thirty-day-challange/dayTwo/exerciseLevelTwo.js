520966e90f091783162116b588d7bc7283f3dbf3
Aamir Azad <aamirmazad@gmail.com>
2022-11-17 19:11:24 -0500

    Testing out git

--- Thirty-day-challange/dayTwo/exerciseLevelTwo.js
+++ Thirty-day-challange/dayTwo/exerciseLevelTwo.js
@@ -1,6 +1,6 @@
-let johnQuote = `'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.`
+const johnQuote = `'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.`
 console.log(johnQuote)
-let teresaQuote = "Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead."
+const teresaQuote = "Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead."
 console.log(teresaQuote);
 
 let number = '10';
